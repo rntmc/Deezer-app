50f1c3e430a6d2361bca2b213480cdddb491c330
renato <renatocarrino@gmail.com>
2024-01-27 15:00:05 -0300

    Creation of Input component

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from '@styles/theme';
 
 import { Button } from "./components/Button"
+import { Input } from "./components/Input"
 
 const Home = () => {
   return (
     <ThemeProvider theme={theme}>
       <Button title="favoritos"/>
+      <Input/>
     </ThemeProvider>
   )
 }
