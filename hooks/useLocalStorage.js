9be36af231fb58390e020452a0cc7a4a05e27ebc
renato <renatocarrino@gmail.com>
2024-01-30 23:06:57 -0300

    LocalStorage adjustments to avoid errors

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 export function useLocalStorage(key, initial) {
-  const isLocalStorageAvailable = typeof window !== 'undefined' && window.localStorage;
   
   const getSavedValue = () => {
     const savedValue = JSON.parse(localStorage.getItem(key));
